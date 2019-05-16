a9c6d2422af79a18e19e6e5ed84bd39fbe1f339b
Ryan Rowland <w.rowland.ryan@gmail.com>
2019-05-16 16:21:50 -0500

    Used ternary operator in Validation component

--- src/Validation/Validation.js
+++ src/Validation/Validation.js
@@ -1,18 +1,13 @@
 import React from 'react'
 
 const Validation = (props) => {
-  let output;
-
-  if (props.length >= props.min) {
-    output = (<p>Text long enough</p>);
-  }
-  else {
-    output = (<p>Text too short</p>);
-  }
-
   return (
     <div>
-      {output}
+      {
+        props.length < props.min ?
+          <p style={{ color: 'red' }}>Text too short</p> :
+          <p style={{ color: 'green' }}>Text long enough</p>
+      }
     </div>
   )
 }
